52e4ab101ddfcf319789a39c163cbc0457cd11b5
tohnk <idiotohnk@gmail.com>
2026-08-12 03:11:19 +0100

    Update epubfix.rb

--- Formula/epubfix.rb
+++ Formula/epubfix.rb
@@ -1,8 +1,8 @@
 class Epubfix < Formula
   desc "A CLI tool to fix and repair EPUB files"
   homepage "https://github.com/tohnk/epubfix"
-  url "https://github.com/tohnk/epubfix/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "c3c506d5becf830df4a9a87fbb2a7ec2583acfeaafb68e4aee76bbcb2789bf85"
+  url "https://github.com/tohnk/epubfix/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "417e4f2eccd68302f57dbe77ca9a51b614962a182fdc75cf54d0e50ae0f0ca30"
   license "MIT" # Update if your repo uses a different license (e.g. "Apache-2.0")
 
   depends_on "rust" => :build
