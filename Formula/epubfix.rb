62338a586935aab539e84d2e329fee3db9c78fb0
tohnk <idiotohnk@gmail.com>
2026-08-19 00:46:00 +0100

    epubfix 0.2.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/epubfix.rb
+++ Formula/epubfix.rb
@@ -1,8 +1,8 @@
 class Epubfix < Formula
   desc "A CLI tool to fix and repair EPUB files"
   homepage "https://github.com/tohnk/epubfix"
-  url "https://github.com/tohnk/epubfix/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "0d45bc2f8b6876bc96618c714a808de3c41d2db0ca50d97d2de5909299dc6a49"
+  url "https://github.com/tohnk/epubfix/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "15a05af96caea7197e6023e778f72035f4bbf76cc07a0d2a95b63a1a6d32e28a"
   license "MIT" # Update if your repo uses a different license (e.g. "Apache-2.0")
 
   depends_on "rust" => :build
